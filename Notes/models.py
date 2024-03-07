a2053e1c4ad873b7ad24974d82948bf5a19b94cc
Sun <40611674+SunLaria@users.noreply.github.com>
2024-03-07 18:21:52 +0200

    Update models.py

--- Notes/models.py
+++ Notes/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
-
 class Note(models.Model):
     text = models.CharField(max_length=255)
     color = models.CharField(max_length=10)
